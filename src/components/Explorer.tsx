9726cea788f98031fc185f90147ef12d93aeaa2d
Felix Erdmann <hello@felixerdmann.com>
2022-10-31 22:18:41 +0100

    weitere infos

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,18 +1,20 @@
 import { Frame, List, Modal } from "@react95/core";
 import { WindowsExplorer } from "@react95/icons";
+import party_seal from "../assets/gifs/party_seal.gif";
+import cool_dog from "../assets/gifs/cool-doge.gif";
+import meow_party from "../assets/gifs/meow_party.gif";
 
 type Props = {
   onClose: () => void;
 };
 
-const Umfrage = ({ onClose }: Props) => {
+const Explorer = ({ onClose }: Props) => {
   return (
     <Modal
       icon={<WindowsExplorer variant="16x16_4" />}
       title="HG09i Explorer"
       closeModal={onClose}
-      width="300"
-      height="200"
+      width="400"
       defaultPosition={{
         x: -50,
         y: 20,
@@ -29,15 +31,49 @@ const Umfrage = ({ onClose }: Props) => {
       ]}
     >
       <h1>HG09i Klassentreffen</h1>
-      <Frame bg="white" boxShadow="in" padding="0px 5px">
+      <Frame bg="white" boxShadow="in" padding="1rem">
+        <div>
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+          <img src={party_seal} style={{ height: "1rem" }} alt="Party Seal" />
+        </div>
         <p>
           Willkommen auf der Info Seite für das HG09i Klassentreffen 2023. Hier
-          halten wir euch auf dem neuesten Stand.
+          halten wir euch auf dem neuesten Stand.{" "}
+          <img src={cool_dog} style={{ height: "1rem" }} alt="Cool doge" />
         </p>
-        <p>Weitere Informationen folgen in Kuerze</p>
+        <h2>
+          Vorlaeufiges Datum: <u>19. August 2023</u>
+        </h2>
+        <p>
+          Wir planen das Event in Q3 2023, rund um Mitte / Ende August 2023.
+          Aenderungen bitte vorbehalten.
+        </p>
+        <h3>Rahmendaten / Auswertung der Umfrage</h3>
+        <ul>
+          <li>
+            Aktion tagsuesber (Start am fruehen Nachmittag) und Abend, keine
+            Uebernachtung
+          </li>
+          <li>NRW / Muensterland / noerdliches Ruhrgebiet</li>
+          <li>Kleine Aktion, Fokus auf geilen Abend</li>
+          <li>Q3 (Juli/August/September)</li>
+          <li>Lehrer:innen werden eingeladen</li>
+        </ul>
       </Frame>
     </Modal>
   );
 };
 
-export default Umfrage;
+export default Explorer;
